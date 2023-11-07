de9af1c3a1fcde9fabb0eb05bb79add0abd70f5b
Pinto Tetteh Aaron <aaronpinto111@gmail.com>
2023-11-07 09:46:37 +0000

    sticking to the ORM method

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
-import uvicorn
-import os
-from dotenv import load_dotenv
-from fastapi import FastAPI, HTTPException, status
-from pydantic import BaseModel
-from typing import Optional
-import psycopg2
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends
+from database import engine, SessionLocal
+import models
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-class Team(BaseModel):
-    name: str
-    number_of_players: Optional[int] = 11
-    coach: str
-
-
-load_dotenv()
-
-try:
-    conn = psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-    )
-    cursor = conn.cursor()
-    print("Connected to database")
-except Exception as e:
-    print("Unable to connect to database", e)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
@@ -35,69 +21,7 @@ def home():
     return {"message": "WELCOME HERE"}
 
 
-@app.get("/teams")
-def get_teams():
-    try:
-        cursor.execute("""SELECT * FROM team """)
-        teams = cursor.fetchall()
-        return {"data": teams}
-    except Exception as error:
-        return {"message": "Unable to fetch teams", "error": error}
-
-
-@app.get("/teams/{team_id}")
-def get_team(team_id: int):
-    try:
-        cursor.execute("""SELECT * FROM team WHERE team_id = %s""", (team_id,))
-        team = cursor.fetchone()
-        return {"data": team}
-    except Exception as error:
-        return {"message": "Unable to fetch team", "error": error}
-
-
-@app.post("/teams", status_code=status.HTTP_201_CREATED)
-def create_team(team: Team):
-    try:
-        cursor.execute(
-            """INSERT INTO team (name, number_of_players, coach) VALUES (%s, %s, %s) RETURNING * """,
-            (team.name, team.number_of_players, team.coach),
-        )
-        new_team = cursor.fetchone()
-        conn.commit()
-        return {"data": new_team}
-    except Exception as error:
-        return {"message": "Unable to create team", "error": error}
-
-
-@app.put("/teams/{team_id}")
-def update_team(team_id: int, new_team: Team):
-    try:
-        cursor.execute("""SELECT * FROM team WHERE team_id = %s""", (team_id,))
-        team = cursor.fetchone()
-        if team:
-            cursor.execute(
-                """UPDATE team SET name = %s, number_of_players = %s, coach = %s WHERE team_id = %s RETURNING * """,
-                (new_team.name, new_team.number_of_players, new_team.coach, team_id),
-            )
-            updated_team = cursor.fetchone()
-            conn.commit()
-            return {"data": updated_team}
-        else:
-            return {"message": "Team not found"}
-    except Exception as error:
-        return {"message": "Unable to update team", "error": error}
-
-
-@app.delete("/teams/{team_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_team(team_id: int):
-    try:
-        cursor.execute("""SELECT * FROM team WHERE team_id = %s""", (team_id,))
-        team = cursor.fetchone()
-        if team:
-            cursor.execute("""DELETE FROM team WHERE team_id = %s""", (team_id,))
-            conn.commit()
-            return {"message": "Team deleted successfully"}
-        else:
-            return {"message": "Team not found"}
-    except Exception as error:
-        return {"message": "Unable to delete team", "error": error}
+@app.get("/customers")
+def get_customers(db: Session = Depends(get_db)):
+    customers = db.query(models.Customer).all()
+    return {"data": customers}
