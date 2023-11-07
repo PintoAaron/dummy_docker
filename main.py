48997905f46137a862bfe449ddca86dedf6f5784
Pinto Tetteh Aaron <aaronpinto111@gmail.com>
2023-11-07 12:14:01 +0000

    created customer pydantic model

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from sqlalchemy.orm import Session
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status
 from database import engine, get_db
 import models
-from models import Customer
+from pydantic_models import CustomerBase
+
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -19,8 +20,8 @@ def get_customers(db: Session = Depends(get_db)):
     return {"data": customers}
 
 
-@app.post("/customers", response_model=None)
-def create_customer(customer: Customer, db: Session = Depends(get_db)):
+@app.post("/customers", status_code=status.HTTP_201_CREATED)
+def create_customer(customer: CustomerBase, db: Session = Depends(get_db)):
     new_customer = models.Customer(name=customer.name, email=customer.email)
     db.add(new_customer)
     db.commit()
