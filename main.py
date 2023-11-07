f084df926583efeb32223a19fd637a18bb151e79
Pinto Tetteh Aaron <aaronpinto111@gmail.com>
2023-11-07 11:38:27 +0000

    modified model

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends
 from database import engine, get_db
 import models
-
+from models import Customer
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -17,3 +17,12 @@ def home():
 def get_customers(db: Session = Depends(get_db)):
     customers = db.query(models.Customer).all()
     return {"data": customers}
+
+
+@app.post("/customers", response_model=None)
+def create_customer(customer: Customer, db: Session = Depends(get_db)):
+    new_customer = models.Customer(name=customer.name, email=customer.email)
+    db.add(new_customer)
+    db.commit()
+    db.refresh(new_customer)
+    return {"data": new_customer}
--- models.py
+++ models.py
@@ -1,11 +1,10 @@
 from database import Base
 import sqlalchemy
-from datetime import datetime
 
 
 class Customer(Base):
     __tablename__ = "customers"
     customer_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    name = sqlalchemy.Column(sqlalchemy.String)
-    email = sqlalchemy.Column(sqlalchemy.String)
-    created_at = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.now())
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    email = sqlalchemy.Column(sqlalchemy.String, nullable=False, unique=True)
+    created_at = sqlalchemy.Column(sqlalchemy.DateTime, server_default=sqlalchemy.func.now(), nullable=False)
