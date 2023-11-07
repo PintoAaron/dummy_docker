1b9a2c8ade6d82c557d0ba234e2bdaca13e2f0ff
Pinto Tetteh Aaron <aaronpinto111@gmail.com>
2023-11-07 10:05:40 +0000

    moved get_db to database.py

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends
-from database import engine, SessionLocal
+from database import engine, get_db
 import models
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 
 @app.get("/")
